2b7a36e4698006e6c1173b61cf324a176e0ed6b4
ovaca2025 <ovaca2025@gmail.com>
2025-02-04 14:03:17 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Student2 on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var message = "I Am A Programmer!"
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                
+                
+                Button("Great!") {
+                    message = "Great!"
+                }
+            }
+            
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Student2 on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
