37bd9ab52d5c791462de8b81e1f6255d0eaf23f5
ovaca2025 <ovaca2025@gmail.com>
2025-02-14 14:58:59 -0800

    Feb 14

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -2,44 +2,64 @@
 //  ContentView.swift
 //  YouAreAwesome
 //
-//  Created by Student2 on 1/27/25.
+//  Created by Student2 on 2/6/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-  @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
+    
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: "swift")
+            
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-                .foregroundStyle(.black)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
-            HStack {
-                Button("Awesome!") {
-                    message = "Awesome!"
-                }
-                
-                
-                Button("Great!") {
-                    message = "Great!"
-                }
-            }
             
+            Button("Show Message") {
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "You're Incredible!",
+                                "I Am So Happy To Have You!"]
+                
+                message = messages[messageNumber]
+                messageNumber += 1
+                if  messageNumber == messages.count {
+                     messageNumber = 0
+                    
+                }
+                
+               imageName = "image\(imageNumber)"
+//                imageNumber = imageNumber + 1
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                print(imageNumber)
+            }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
             
         }
         .padding()
